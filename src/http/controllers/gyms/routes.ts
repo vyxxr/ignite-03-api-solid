1d43e9900c4fe0a401565e571ddf56897b96d74d
vyxxr <vyctor@tuta.io>
2023-08-03 14:06:57 -0300

    feat: add refresh token

--- src/http/controllers/gyms/routes.ts
+++ src/http/controllers/gyms/routes.ts
@@ -3,12 +3,15 @@ import { verifyJWT } from '@/http/middlewares/verify-jwt'
 import { search } from './search'
 import { nearby } from './nearby'
 import { create } from './create'
+import { refresh } from '../users/refresh'
 
 export async function gymsRoutes(app: FastifyInstance) {
   app.addHook('onRequest', verifyJWT)
 
   app.get('/gyms/search', search)
   app.get('/gyms/nearby', nearby)
+
+  app.patch('/token/refresh', refresh)
 
   app.post('/gyms', create)
 }
